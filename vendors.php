6e53e8c66861a597d9683af990f4cd80609f77a4
Francis Besset <francis.besset@gmail.com>
2011-07-03 10:19:33 +0200

    Updated vendors script to run test suite
    ref: https://github.com/BeSimple/I18nRoutingBundle/commit/55ee41cb842f63513b02b887bc77c124cefbce13#commitcomment-456777

---
+++ vendors.php
@@ -0,0 +1,41 @@
+#!/usr/bin/env php
+<?php
+
+/*
+ * This file is part of the Symfony framework.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/*
+
+CAUTION: This file installs the dependencies needed to run the I18nRoutingBundle test suite.
+If you want to create a new project, download the Symfony Standard Edition instead:
+
+https://github.com/BeSimple/I18nRoutingBundle
+
+*/
+
+if (!is_dir($vendorDir = dirname(__FILE__).'/vendor')) {
+    mkdir($vendorDir, 0777, true);
+}
+
+$deps = array(
+    array('symfony', 'http://github.com/symfony/symfony.git', 'origin/HEAD'),
+);
+
+foreach ($deps as $dep) {
+    list($name, $url, $rev) = $dep;
+
+    echo "> Installing/Updating $name\n";
+
+    $installDir = $vendorDir.'/'.$name;
+    if (!is_dir($installDir)) {
+        system(sprintf('git clone %s %s', escapeshellarg($url), escapeshellarg($installDir)));
+    }
+
+    system(sprintf('cd %s && git fetch origin && git reset --hard %s', escapeshellarg($installDir), escapeshellarg($rev)));
+}
