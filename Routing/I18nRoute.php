01781b24fbbe08b1711ed86f2d6ad6156ee79529
Roger Llopart Pla <lumbendil@gmail.com>
2013-10-16 10:34:25 +0200

    Update I18nRoute.php

--- Routing/I18nRoute.php
+++ Routing/I18nRoute.php
@@ -29,7 +29,7 @@ class I18nRoute
         foreach ($locales as $locale => $pattern) {
             $defaults['_locale'] = $locale;
 
-            $this->collection->add($name.'.'.$locale, new Route($pattern, $defaults, $requirements, $options, true));
+            $this->collection->add($name.'.'.$locale, new Route($pattern, $defaults, $requirements, $options));
         }
     }
 
