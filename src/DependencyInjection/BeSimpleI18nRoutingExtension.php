b3697f47046eca82ad4666938f298ddcb318ab5e
Warnar Boekkooi <warnar@boekkooi.net>
2016-03-31 11:46:10 +0200

    Use PSR-4 instead of PSR-0

--- src/DependencyInjection/BeSimpleI18nRoutingExtension.php
+++ src/DependencyInjection/BeSimpleI18nRoutingExtension.php
@@ -4,9 +4,7 @@ namespace BeSimple\I18nRoutingBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
-use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class BeSimpleI18nRoutingExtension extends Extension
