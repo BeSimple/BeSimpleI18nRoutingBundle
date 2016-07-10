d5209be0b37e435ea68c51c28906be94e0145aa9
Jules Pietri <heah@heahprod.com>
2016-07-10 21:19:23 +0200

    added missing test for MissingRouteLocaleException

--- tests/Routing/Loader/AnnotatedRouteControllerLoaderTest.php
+++ tests/Routing/Loader/AnnotatedRouteControllerLoaderTest.php
@@ -96,4 +96,15 @@ class AnnotatedRouteControllerLoaderTest extends \PHPUnit_Framework_TestCase
 
         $loader->load('BeSimple\I18nRoutingBundle\Tests\Fixtures\Controller\MissingPrefixedLocalesController');
     }
+
+    /**
+     * @expectedException \BeSimple\I18nRoutingBundle\Routing\Exception\MissingRouteLocaleException
+     */
+    public function testRoutesMissingLocales()
+    {
+        $loader = new AnnotatedRouteControllerLoader(new AnnotationReader());
+        AnnotationRegistry::registerLoader('class_exists');
+
+        $loader->load('BeSimple\I18nRoutingBundle\Tests\Fixtures\Controller\MissingLocalesController');
+    }
 }
